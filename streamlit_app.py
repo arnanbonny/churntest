44d2b73b893cddaa6f078dc511017048c64de07b
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-19 22:59:58 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ st.write('Hellow World!')
 # Single sheet by position (0-based)
 df_demographics = pd.read_excel("churn.xlsx", sheet_name=0)
 
+with st.expander('Patu'): 
+  sns.catplot(data = df_demographics_c, x ='ChurnStatus', y ='Age',hue = 'IncomeLevel', kind = 'box')
+  plt.show()
