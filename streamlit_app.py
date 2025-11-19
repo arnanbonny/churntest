763ec7244025ffc62b548ff3d221f6c6ce1e0c51
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-19 23:26:40 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,17 @@ st.write('Hellow World!')
 # Single sheet by position (0-based)
 df_demographics = pd.read_excel("churn.xlsx", sheet_name=1)
 
+
+# Assuming df_demographics is already loaded
+
 with st.sidebar:
     st.header('Filters')
-    Category = st.multiselect('Category', df_demographics['ProductCategory'].unique())
+    Categories = st.multiselect('Category', df_demographics['ProductCategory'].unique(), default=df_demographics['ProductCategory'].unique())
 
-# Filter the dataframe based on the selected category
-filtered_df = df_demographics[df_demographics['ProductCategory'] == Category]
+# Filter dataframe for all selected categories
+filtered_df = df_demographics[df_demographics['ProductCategory'].isin(Categories)]
 
 with st.expander('Patu'):
-    st.bar_chart(data=filtered_df, x='ProductCategory', y='AmountSpent')
+    st.bar_chart(data=filtered_df, x='ProductCategory', y
+
 
