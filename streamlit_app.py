7f7dee1869f863f49d80ea07b7d508183e77d4a0
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-19 23:18:09 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ st.write('Hellow World!')
 df_demographics = pd.read_excel("churn.xlsx", sheet_name=1)
 
 with st.expander('Patu'): 
-  st.scatter_chart(data=df_demographics, x='ProductCategory', y='AmountSpent')
+  st.bar_chart(data=df_demographics, x='ProductCategory', y='AmountSpent')
   plt.show()
