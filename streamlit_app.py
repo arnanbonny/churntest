af60700c547009371edea8e6df705ad979cebdb1
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-19 22:46:46 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ import seaborn as sns
 st.title('Arnan Bonny')
 st.write('Hellow World!')
 # Single sheet by position (0-based)
-df_demographics = pd.read_excel(r"E:\Data Science and Analytics\PROJECT 1 - CUSTOMER CHURN\Customer_Churn_Data_Large  Lloyds.xlsx", sheet_name=0) 
+df_demographics = pd.read_excel('data/Customer_Churn_Data_Large_Lloyds.xlsx', sheet_name=0) 
