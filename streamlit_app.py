ea056897090821d9d1ec55c22c417c12eceb7d6b
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-19 23:16:45 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import seaborn as sns
 st.title('Arnan Bonny')
 st.write('Hellow World!')
 # Single sheet by position (0-based)
-df_demographics = pd.read_excel("churn.xlsx", sheet_name=0)
+df_demographics = pd.read_excel("churn.xlsx", sheet_name=1)
 
 with st.expander('Patu'): 
-  st.bar_chart(data=df_demographics, x='IncomeLevel', y='Age', color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
+  st.scatter_chart(data=df_demographics, x='ProductCategory', y='AmountSpent', color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
   plt.show()
